ee486339de203639027f0a5427e48037097d2959
m_sahraei <mahsasahraei71@gmail.com>
2024-05-23 09:37:28 +0330

    without redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementHandler = () => {
+    setCounter((counter) => counter + 1);
+  };
+
+  const decrementHandler = () => {
+    setCounter((counter) => counter - 1);
+  };
+
+  const resetCounter = () => {
+    setCounter(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <h1 id="num">{counter}</h1>
+      <div class="btns">
+        <button class="dec" onClick={decrementHandler}>
+          <i class="fas fa-minus"></i>
+        </button>
+        <button class="reset" onClick={resetCounter}>
+          <i class="fas fa-redo"></i>
+        </button>
+        <button class="inc" onClick={incrementHandler}>
+          <i class="fas fa-plus"></i>
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
